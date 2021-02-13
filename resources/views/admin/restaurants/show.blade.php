1533b4b04d3cc1ee3db93f0307deda215d0d80e3
degortes <danilodegortes@gmal.com>
2021-02-13 16:36:03 +0100

    Aggiunti dettagli alla show

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -4,29 +4,35 @@
 <ul>
 
     <li>
-        nome: {{$restaurant->name}}
+        nome Ristorante: {{$restaurant->name}}
     </li>
     <li>
 
         indirizzo: {{$restaurant->address}}
     </li>
 
-        <li>
-
+    <li>
         telefono: {{$restaurant->phone}}
     </li>
-    <ul>
+</ul>
+<br>
 
     @foreach ($restaurant->dishes as $dish)
+    <ul>
         <li>
             Nome piatto: {{$dish->name}}
         </li>
-
+        <li>
+            Ingredienti: {{$dish->ingredients}}
+        </li>
+        <li>
+            Prezzo: {{$dish->price}}
+        </li>
+    </ul>
+    <br>
     @endforeach
 
-    </ul>
 
 
-</ul>
 
 @endsection

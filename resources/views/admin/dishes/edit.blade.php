3049bbd2b5beb1e70cb989cf2b78ca43eabb3263
Giacomo <giacomo.1936@gmail.com>
2021-02-15 17:35:19 +0100

    modifica edit

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-12">
             <div class="d-flex justify-content-between align-items-center">
-                <h1>Modifica post: {{ $post->title }}</h1>
+                <h1>Modifica post: {{ $dish->title }}</h1>
                 <a href="{{ route('admin.posts.index') }}" class="btn btn-primary">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><line x1="20" y1="12" x2="4" y2="12"></line><polyline points="10 18 4 12 10 6"></polyline></svg> Tutti i posts
                 </a>

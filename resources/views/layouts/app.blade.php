d1671f1adc6eff85696eeeff0def59242c3851ea
Manuel <manuel.devito@icloud.com>
2021-02-16 12:26:59 +0100

    commit prova logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <!-- Media Query -->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0 ">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">

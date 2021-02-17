27163b009df34ac274780cdbe0c8aba36a4e474b
Furio Alessio <alefurio@hotmail.it>
2021-02-17 01:41:50 +0100

    layout dashboard + card-restaurant

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -2,62 +2,53 @@
 
 @section('content')
 
-
-<div class="container">
-    <div class="row">
-        <h1>I tuoi Ristoranti:</h1>
+<div class="dashboard">
+    <div class="dashboard-header">
+        <h1>Dashboard</h1>
     </div>
-    <div class="row">
-        <div class="text-center">
-            <a href="{{route('admin.restaurants.create')}}" class="btn btn-primary">Aggiungi Ristorante</a>
+    <div class="dashboard-body">
+        <div class="dashboard-body-menu">
+            <ul>
+                <li>
+                    <a href="{{route('admin.restaurants.create')}}" class="btn add-restaurant">Aggiungi Ristorante</a>
+                </li>
+            </ul>
         </div>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Indirizzo</th>
-                    <th>Telefono</th>
-                    <th>azioni</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($restaurants as $restaurant)
-                    <tr>
-                        <td>{{$restaurant->id}}</td>
-                        <td> <a href="{{route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}">{{$restaurant->name}}</a></td>
-                        <td>{{$restaurant->address}}</td>
-                        <td>{{$restaurant->phone}}</td>
-                        <td><a href="{{route('admin.restaurants.show' , ['restaurant' => $restaurant->id ] )}}" class="btn btn-primary">Dettagli</a></td>
-                        <td><a href="{{route('admin.restaurants.edit' , ['restaurant' => $restaurant->id ] )}}" class="btn btn-warning">Modifica</a></td>
-                        {{-- <td>
-                            <form action="{{route('admin.restaurants.destroy' , ['restaurant' => $restaurant->id ] )}}" method="post">
-                                <button type="submit" name="button" class="btn btn-danger">Elimina</button>
-                                @csrf
-                                @method('DELETE')
-                            </form>
-                        </td> --}}
-                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="dashboard-body-content">
+            <div class="container-restaurant">
+                <div class="row row-title">
+                    <h1>I tuoi Ristoranti:</h1>
+                </div>
+                <div class="row">
 
+                </div>
 
-
-                {{-- <div class="body-restaurant-card">
-                    <div class="details-restaurant-card">
-                        <p>ID: <strong>{{ $restaurant->id }}</strong></p>
-                        <p>Indirizzo: <strong>{{ $restaurant->address }}</strong></p>
-                        <p>Telefono:</p> <strong>{{ $restaurant->phone }}</strong></p>
-                    </div>
-                    <div class="action-restaurant-card">
-                        <div class="container-btn-action">
-                            <a href="{{route('admin.restaurants.show' , ['restaurant' => $restaurant->id ] )}}" class=" btn btn-dettagli">Dettagli</a>
-                            <a href="{{route('admin.restaurants.edit' , ['restaurant' => $restaurant->id ] )}}" class="btn btn-warning btn-modifica">Modifica</a>
+                <div class="restaurants">
+                    @foreach ($restaurants as $restaurant)
+                    <div class="restaurant-card">
+                        <div class="title-restaurant-card">
+                            <h4>Nome ristorante: </h4> 
+                            <h1>{{ $restaurant->name }}</h1>
+                        </div>
+                        <div class="body-restaurant-card">
+                            <div class="details-restaurant-card">
+                                <p>ID: <strong>{{ $restaurant->id }}</strong></p>
+                                <p>Indirizzo: <strong>{{ $restaurant->address }}</strong></p>
+                                <p>Telefono:</p> <strong>{{ $restaurant->phone }}</strong></p>
+                            </div>
+                            <div class="action-restaurant-card">
+                                <div class="container-btn-action">
+                                    <a href="{{route('admin.restaurants.show' , ['restaurant' => $restaurant->id ] )}}" class="btn btn-primary btn-dettagli">Dettagli</a>
+                                    <a href="{{route('admin.restaurants.edit' , ['restaurant' => $restaurant->id ] )}}" class="btn btn-warning btn-modifica">Modifica</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div> --}}
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 

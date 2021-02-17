35eca7631d9786ec3204cb615f2a7e5542123c19
Furio Alessio <alefurio@hotmail.it>
2021-02-17 15:26:52 +0100

    cartella layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Deliveroo') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Media Query -->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0 ">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar">
+            <div class="container-general-navbar">
+                <div class="container-brand-navbar">
+                    <div class="wp-navbar-brand">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="/images/logo.png" alt="">
+                        </a>
+                    </div>
+                    {{-- <div class="wp-navbar-brand">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="/images/logo.png" alt="">
+                        </a>
+                    </div> --}}
+                </div>
+                <div class="container-login-register">
+                    <div class="navbar-nav">
+
+                        @guest
+                            <div class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </div>
+                            @if (Route::has('register'))
+                                <div class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </div>
+                            @endif
+                        @else
+                            <div class="nav-item">
+                                <div class="admin">
+                                    <a id="navbarDropdown" class="nav-link" href="#">
+                                        {{ Auth::user()->name }}
+                                    </a>
+                                </div>
+
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </div>
+                        @endguest
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>

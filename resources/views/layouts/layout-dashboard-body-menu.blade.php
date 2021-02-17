f53c840d70fc954828729551d479e6e69bea7789
Furio Alessio <alefurio@hotmail.it>
2021-02-17 19:08:56 +0100

    layout-dashboard-menu

---
+++ resources/views/layouts/layout-dashboard-body-menu.blade.php
@@ -0,0 +1,13 @@
+<div class="dashboard-body-menu">
+    <ul>
+        <li>
+            <a href="{{route('admin.restaurants.index')}}" class="btn add-restaurant">I Tuoi Ristoranti</a>
+        </li>
+        <li>
+            <a href="{{route('admin.restaurants.create')}}" class="btn add-restaurant">Aggiungi Ristorante</a>
+        </li>
+        <li>
+            <a href="{{route('admin.dishes.create')}}" class="btn add-restaurant">Aggiungi Piatto</a>
+        </li>
+    </ul>
+</div>

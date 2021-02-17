2633bc8fd9330cb02ab65acdc98bf28cf3abca19
Manuel <manuel.devito@icloud.com>
2021-02-17 12:38:19 +0100

    pagina order restaurant modificata

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,2 +1,55 @@
-<h1>Lista Ordini</h1>
-<a href="#">Statistiche Ordini</a>
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="dashboard">
+        <div class="dashboard-header">
+            <h1>Dashboard</h1>
+        </div>
+        <div class="dashboard-body">
+            <div class="dashboard-body-menu">
+                <ul>
+                    <li>
+                        <a href="#" class="btn add-restaurant">Aggiungi Ordine</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="dashboard-body-content">
+                <div class="container-restaurant">
+                    <div class="row row-title">
+                        <h1>I tuoi Ordini:</h1>
+                    </div>
+                    <div class="row">
+
+                    </div>
+
+                    <div class="restaurants">
+                        {{-- @foreach ( as ) --}}
+                        <div class="restaurant-card">
+                            <div class="title-restaurant-card">
+                                <h4>Ordine</h4>
+                                <h1></h1>
+                            </div>
+                            <div class="body-restaurant-card">
+                                <div class="details-restaurant-card">
+                                    <p></p>
+                                    <p></p>
+                                    <p></p>
+                                </div>
+                                <div class="action-restaurant-card">
+                                    <div class="container-btn-action">
+                                        <a href="#" class="btn btn-primary btn-dettagli">Dettagli</a>
+                                        <a href="#" class="btn btn-warning btn-modifica">Modifica</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- @endforeach --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection

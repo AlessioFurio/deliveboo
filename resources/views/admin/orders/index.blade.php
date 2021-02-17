a646d212f02dd48ca6e7dd6a3fdce88873ba1d55
Manuel <manuel.devito@icloud.com>
2021-02-17 23:16:11 +0100

    sistemata pagina admin order

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -2,55 +2,47 @@
 
 @section('content')
 
-    <div class="dashboard">
-        <div class="dashboard-header">
+    <div class="dashboard-order">
+        <div class="dashboard-header-order">
             <h1>Dashboard</h1>
         </div>
-        <div class="dashboard-body">
-            <div class="dashboard-body-menu">
-                <ul>
-                    <li>
-                        <h1>Lista Ristoranti</h1>
-                        <ul>
-                            @foreach ($restaurants as $restaurant)
-                                <li>
-                                    <a href="{{ route('admin.payments.index') }}">{{ $restaurant->name }}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" class="btn add-restaurant">Aggiungi Ordine</a>
-                    </li>
-                </ul>
+        <div class="dashboard-body-order">
+            <div class="dashboard-body-menu-order">
+                <h1 class="title-restaurant">Lista Ristoranti</h1>
+                <ol>
+                    @foreach ($restaurants as $restaurant)
+                        <li>
+                            <a href="{{ route('admin.payments.index') }}">{{ $restaurant->name }}</a>
+                        </li>
+                    @endforeach
+                </ol>
+                {{-- <div>
+                    <a href="#" class="btn add-restaurant">Aggiungi Ordine</a>
+                </div> --}}
             </div>
 
-            <div class="dashboard-body-content">
-                <div class="container-restaurant">
-                    <div class="row row-title">
+            <div class="dashboard-body-content-order">
+                <div class="container-restaurant-order">
+                    <div class="order-title">
                         <h1>I tuoi Ordini:</h1>
                     </div>
-                    <div class="row">
-
-                    </div>
-
-                    <div class="restaurants">
+                    <div class="restaurants-order">
                         {{-- @foreach ( as ) --}}
-                        <div class="restaurant-card">
-                            <div class="title-restaurant-card">
+                        <div class="restaurant-card-order">
+                            <div class="title-restaurant-card-order">
                                 <h4>Ordine</h4>
                                 <h1></h1>
                             </div>
-                            <div class="body-restaurant-card">
-                                <div class="details-restaurant-card">
+                            <div class="body-restaurant-card-order">
+                                <div class="details-restaurant-card-order">
                                     <p></p>
                                     <p></p>
                                     <p></p>
                                 </div>
-                                <div class="action-restaurant-card">
-                                    <div class="container-btn-action">
-                                        <a href="#" class="btn btn-primary btn-dettagli">Dettagli</a>
-                                        <a href="#" class="btn btn-warning btn-modifica">Modifica</a>
+                                <div class="action-restaurant-card-order">
+                                    <div class="container-btn-action-order">
+                                        <a href="#" class="btn btn-dettagli">Dettagli</a>
+                                        <a href="#" class="btn btn-modifica">Modifica</a>
                                     </div>
                                 </div>
                             </div>

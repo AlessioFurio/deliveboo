1dae17a4147577cfe4c7108c951c9fe7f54a5856
Roberta <robertasciortino.ag@gmail.com>
2021-02-17 11:39:19 +0100

    stile card piatti

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,15 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+  <div class="dashboard">
+      <div class="dashboard-header">
+          <h1>Dashboard</h1>
+      </div>
+      <div class="dashboard-body">
+          <div class="dashboard-body-menu">
+              <ul>
+                  <li>
+                      <a href="{{route('admin.restaurants.create')}}" class="btn add-restaurant">Aggiungi Ristorante</a>
+                  </li>
+                  <li>
+                      <a href="{{route('admin.dishes.create')}}" class="btn add-restaurant">Aggiungi Piatto</a>
+                  </li>
+              </ul>
+          </div>
+
+          <div class="dashboard-body-content">
+              <div class="container-restaurant">
+                  <div class="row row-title">
+                      <h1>I tuoi piatti:</h1>
+                  </div>
+                  <div class="row">
+
+                  </div>
+
+                  <div class="restaurants">
+                      @foreach ($restaurants as $restaurant)
+                        @foreach  ($restaurant->dishes as $dish)
+
+                          <div class="restaurant-card">
+                            <div class="title-restaurant-card">
+                              <h4>Nome ristorante: </h4>
+                              <h1>{{ $dish->name }}</h1>
+                            </div>
+                            <div class="body-restaurant-card">
+                              <div class="details-restaurant-card">
+
+                                <p>ID: <strong>{{ $dish->id }}</strong></p>
+                                <p>Nome piatto: <strong>{{ $dish->name }}</strong></p>
+                                <p>Ingredienti:</p> <strong>{{ $dish->ingredients }}</strong></p>
+                                <p>Portata:</p> <strong>{{ $dish->course->name }}</strong></p>
+                                <p>Visibilita':</p>
+                                @if ($dish->visibility)
+                                  <strong>Disponibile</strong>
+                                @else
+                                  <strong>Non disponibile</strong>
+                                @endif
+
+                                <p>Prezzo:</p> <strong>{{ $dish->price }}</strong></p>
+                              </div>
+                              <div class="action-restaurant-card">
+                                <div class="container-btn-action">
+                                  <a href="{{route('admin.restaurants.show' , ['restaurant' => $restaurant->id ] )}}" class="btn btn-primary btn-dettagli">Dettagli</a>
+                                  <a href="{{route('admin.restaurants.edit' , ['restaurant' => $restaurant->id ] )}}" class="btn btn-warning btn-modifica">Modifica</a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        @endforeach
+                      @endforeach
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-12">
             <div class="d-flex justify-content-between align-items-center">
-                <h1>Tutti i piatti</h1>
+                <h1>I tuoi piatti</h1>
                 <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary">
                     Crea nuovo piatto
                 </a>
             </div>
+
+
             <table class="table-dishes">
                 <thead>
                     <tr>
@@ -49,5 +117,5 @@
             </table>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection

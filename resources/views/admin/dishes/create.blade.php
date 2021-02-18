7fa1d3bb91d761f86c79281f049b7fe2d96fe588
Manuel <manuel.devito@icloud.com>
2021-02-18 18:11:49 +0100

    stilizzata pagina creazione nuovo piatto

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -25,7 +25,7 @@
             <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('POST')
-                <div class="form form-group-plate">
+                <div class="list list-plate">
                     @if (isset($_GET['rest']) && in_array($_GET['rest'], $own_id))
                     <input type="hidden" name="restaurant_id" value="{{$restaurants}}" >
                     @else
@@ -38,27 +38,19 @@
                     </select>
                     @endif
                 </div>
-                <div class="form-group col-12">
-                    <input type="file" name="cover" placeholder="Carica immagine" class="form-control-file">
-
-                    @error('cover')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                <div class="form form-group-plate">
+                <div class="list list-plate">
                     <label>Nome</label>
                     <input class="input" type="text" name="name" placeholder=" Inserisci il nome del piatto..." value=" {{ old('name')}}" required>
                 </div>
-                <div class="form form-group-plate">
+                <div class="list list-plate">
                     <label>Ingredienti</label>
                     <textarea class="textarea input" name="ingredients" rows="10" placeholder=" Inserisci gli ingredienti..."  required>{{ old('ingredients') }}</textarea>
                 </div>
-                <div class="form form-group-plate">
+                <div class="list list-plate">
                     <label>Prezzo</label>
                     <input class="input" type="text" name="price" placeholder=" Inserisci il prezzo..." value="{{ old('price') }}" required>
                 </div>
-                <div class="form form-group-plate">
+                <div class="list list-plate">
                     <label>Portate</label>
                     <select class="input" name="course_id">
                         <option value="">seleziona portata</option>
@@ -69,13 +61,20 @@
                         @endforeach
                     </select>
                 </div>
-                <div class="form-group-plate">
+                <div class="list-plate">
                     <input class="check" type="radio" name="visibility" value="1">
                     <label for="visible">Imposta a visibile</label>
                 </div>
-                <div class="form-group-plate">
+                <div class="list-plate">
                     <input class="check" type="radio" name="visibility" value="0">
                     <label for="no-visible">Imposta a non visibile</label>
+                </div>
+                <div class="list list-plate">
+                    <input class="input-search" type="file" name="cover" placeholder="Carica immagine" class="form-control-file">
+
+                    @error('cover')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="button-create-plate">
                     <button type="submit">

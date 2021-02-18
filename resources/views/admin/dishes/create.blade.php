272a11d98af88b1439be48c3014b9ae0cdae393e
Manuel <manuel.devito@icloud.com>
2021-02-18 17:46:51 +0100

    Auto stash before merge of "main" and "origin/main"

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            <div class="d-flex justify-content-between align-items-center">
-                <h1>Creazione nuovo piatto</h1>
-                <a href="{{ route('admin.dishes.index') }}" class="btn btn-primary">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><line x1="20" y1="12" x2="4" y2="12"></line><polyline points="10 18 4 12 10 6"></polyline></svg>Tutti i piatti
+<div class="container-general-create">
+    <div class="container-general-create-plate">
+        <div class="container-form-create-plate">
+            <div class="title-create-plate">
+                <h1>Crea nuovo piatto</h1>
+            </div>
+            <div class="link-all-plate">
+                <a class="link-plate" href="{{ route('admin.dishes.index') }}">
+                    {{-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><line x1="20" y1="12" x2="4" y2="12"></line><polyline points="10 18 4 12 10 6"></polyline></svg> --}}
+                    Tutti i piatti
                 </a>
             </div>
             @if ($errors->any())
@@ -22,12 +25,12 @@
             <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('POST')
-                <div class="form-group">
+                <div class="form form-group-plate">
                     @if (isset($_GET['rest']) && in_array($_GET['rest'], $own_id))
                     <input type="hidden" name="restaurant_id" value="{{$restaurants}}" >
                     @else
                     <label>Scegli il ristorante</label>
-                    <select class="" name="restaurant_id">
+                    <select class="input" name="restaurant_id">
                         @foreach ($restaurants as $restaurant)
                             <option value="{{$restaurant->id}}" {{ old('restaurant_id') == $restaurant->id ? "selected=selected" : '' }}>{{$restaurant->name}}</option>
                         @endforeach
@@ -43,20 +46,21 @@
                     @enderror
                 </div>
                 <div class="form-group">
+                <div class="form form-group-plate">
                     <label>Nome</label>
-                    <input type="text" name="name" class="form-control" placeholder="Inserisci il nome del piatto" value=" {{ old('name') }}" required>
+                    <input class="input" type="text" name="name" placeholder=" Inserisci il nome del piatto..." value=" {{ old('name')}}" required>
                 </div>
-                <div class="form-group">
+                <div class="form form-group-plate">
                     <label>Ingredienti</label>
-                    <textarea name="ingredients" class="form-control" rows="10" placeholder="Inizia a scrivere qualcosa..."  required>{{ old('ingredients') }}</textarea>
+                    <textarea class="textarea input" name="ingredients" rows="10" placeholder=" Inserisci gli ingredienti..."  required>{{ old('ingredients') }}</textarea>
                 </div>
-                <div class="form-group">
+                <div class="form form-group-plate">
                     <label>Prezzo</label>
-                    <input type="text" name="price" class="form-control" placeholder="Prezzo" value="{{ old('price') }}" required>
+                    <input class="input" type="text" name="price" placeholder=" Inserisci il prezzo..." value="{{ old('price') }}" required>
                 </div>
-                <div class="form-group">
+                <div class="form form-group-plate">
                     <label>Portate</label>
-                    <select class="form-control" name="course_id">
+                    <select class="input" name="course_id">
                         <option value="">seleziona portata</option>
                         @foreach ($courses as $course)
                             <option value="{{ $course->id }}" {{ old('course_id') == $course->id ? "selected=selected" : '' }}>
@@ -64,15 +68,19 @@
                             </option>
                         @endforeach
                     </select>
-                <div class="form-group">
-                    <input type="radio" name="visibility" value="1">
+                </div>
+                <div class="form-group-plate">
+                    <input class="check" type="radio" name="visibility" value="1">
                     <label for="visible">Imposta a visibile</label>
-                    <input type="radio" name="visibility" value="0">
+                </div>
+                <div class="form-group-plate">
+                    <input class="check" type="radio" name="visibility" value="0">
                     <label for="no-visible">Imposta a non visibile</label>
                 </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-success">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg> Crea Piatto
+                <div class="button-create-plate">
+                    <button type="submit">
+                        {{-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg>  --}}
+                        Crea Piatto
                     </button>
                 </div>
             </form>

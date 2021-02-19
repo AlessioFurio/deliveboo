e66a6e03e6d75c07d04ecdbabb61b0b8160431e7
degorts <danilodegortes@gmil.com>
2021-02-19 14:50:25 +0100

    risolto conflitto link

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -3,30 +3,24 @@
 @section('content')
     <section class="card-category">
         <div class="container-card-category">
-
             <div class="menu-filter">
-
                 <h3 class="ricerca-avanzata">Ricerca Avanzata</h3>
-
-                    <select class="category-select" name="" v-model="selectedCategory">
+                <select class="category-select" name="" v-model="selectedCategory">
                         <option value="">Categoria</option>
-                        @foreach ($categories as $category)
-
-                          <option :value="{{$category->id}}">{{$category->name}}</option>
-
-                        @endforeach
+                            @foreach ($categories as $category)
+                              <option :value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
                     </select>
-
                     <button @click="searchRestaurants()" type="button" name="button">Cerca</button>
             </div>
-
             <div class="container-card">
-
                 <div class="cards">
-                        <div class="card" v-for="restaurant in restaurants">
+                    <div class="card" v-for="restaurant in restaurants">
+                        <a :href="'{{url('/restaurants')}}'+'/'+ restaurant.slug">
                             <img src="/images/card.jpg" alt="">
                             <p>@{{ restaurant.name }}</p>
-                        </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>

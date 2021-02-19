389544094b4cec4739b8541b7dd2ecd1c875c609
Furio Alessio <alefurio@hotmail.it>
2021-02-19 12:23:25 +0100

    corretto

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -1,2 +1,34 @@
 @extends('layouts.app-guest')
-@include('partials.section-card-category')
+
+@section('content')
+    <section class="card-category">
+        <div class="container-card-category">
+
+            <div class="menu-filter">
+
+                <h3 class="ricerca-avanzata">Ricerca Avanzata</h3>
+
+                    <select class="category-select" name="" v-model="selectedCategory">
+                        <option value="">Categoria</option>
+                        @foreach ($categories as $category)
+
+                          <option :value="{{$category->id}}">{{$category->name}}</option>
+
+                        @endforeach
+                    </select>
+
+                    <button @click="searchRestaurants()" type="button" name="button">Cerca</button>
+            </div>
+
+            <div class="container-card">
+
+                <div class="cards">
+                        <div class="card" v-for="restaurant in restaurants">
+                            <img src="/images/card.jpg" alt="">
+                            <p>@{{ restaurant.name }}</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection

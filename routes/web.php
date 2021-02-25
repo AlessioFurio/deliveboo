aa59c15906a08d11bc42799a53e35ccf1c08bdc8
degorts <danilodegortes@gmil.com>
2021-02-25 16:46:54 +0100

    contacts page

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 //guest
 Route::get('/', 'HomeController@index')->name('welcome');
-// Route::get('/categories', 'CategoryController@index')->name('categories.index');
-// Route::get('/categories/{slug}', 'CategoryController@show')->name('categories.show');
+Route::get('/contacts', 'HomeController@contacts')->name('contacts');
 Route::get('/restaurants', 'RestaurantController@index')->name('restaurants.index');
 Route::get('/restaurants/{slug}', 'RestaurantController@show')->name('restaurants.show');
-// Route::get('/dishes', 'DishesController@index')->name('dishes.index');
-// Route::get('/dishes/{slug}', 'DishesController@show')->name('dishes.show');
-
 Route::get('/payments', 'PaymentController@index')->name('payments.index');
 Route::post('/transaction', 'PaymentController@transaction')->name('transaction');
 
@@ -32,12 +28,10 @@ Route::middleware('auth')->name('admin.')->namespace('Admin')->prefix('admin')->
   Route::get('/', 'HomeController@index')->name('home');
   Route::get('/chart/{id}', 'PaymentController@chart')->name('statistics.chart');
   Route::get('/chart/{id}/{month}', 'PaymentController@month')->name('statistics.month');
-  // Route::get('/orders', 'OrderController@index')->name('orders.index');
   Route::get('/payments', 'PaymentController@index')->name('orders.index');
   Route::get('/orders/{id}', 'PaymentController@show')->name('orders.show');
   Route::get('/details/{id}', 'PaymentController@details')->name('orders.details');
   Route::get('/statistics/{id}', 'StatisticsController@show')->name('statistics.show');
-  // Route::resource('/categories', 'CategoryController');
   Route::resource('/restaurants', 'RestaurantController');
   Route::resource('/dishes', 'DishController');
 });

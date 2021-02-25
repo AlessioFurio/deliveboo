70021346d6a33c4f4b23e8628646b45d0cac15e7
Giacomo <giacomo.1936@gmail.com>
2021-02-25 18:18:24 +0100

    modifica restaurant guest - aggiunta svuota carrello

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -22,6 +22,7 @@
                                     <li v-for="product in cartCookie" :key="product.id">@{{ product.name }} (@{{ product.quantity }})</li>
                                     <li id="no-border">Prezzo totale: <span>@{{Math.round(totalPrice * 100)/100}} €</span></li>
                                     <a v-if="cartCookie.length != 0" href="{{ route('payments.index') }}">Paga ora</a>
+                                    <a id="btn-clear" @click="Clear">Svuota carrello</a>
                                 </ul>
                             </div>
                         </div>

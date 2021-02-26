02ceba0debe50e6b9ca10332da8384e259faaa9a
Roberta <robertasciortino.ag@gmail.com>
2021-02-26 18:14:43 +0100

    fine modifiche statistiche

--- resources/views/admin/statistics/month.blade.php
+++ resources/views/admin/statistics/month.blade.php
@@ -2,10 +2,8 @@
 @extends('layouts.app')
 
 @section('chart-css')
-
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="{{ asset('css/chart.css') }}" rel="stylesheet">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
-  {{-- <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> --}}
 
 @endsection
 
@@ -14,17 +12,17 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-xs-12">
-        <div>
-          <a href="{{route('admin.statistics.chart', ['id'=> $id])}}" class="btn mt-5 mb-3">Vedi tutti gli ordini</a>
+        <div class="chart-buttons">
           <h2 class="mt-5 mb-3">Filtra per mese</h2>
-          @foreach ($months as $key => $month)
-              <a href="{{route('admin.statistics.month', ['id' => $id , 'month' => $key+1 ])}}" class="btn">{{$month}}</a>
-          @endforeach
+          <a href="{{route('admin.statistics.chart', ['id'=> $id])}}" class="btn btn-outline-dark mt-5 mb-3">Vedi tutti gli ordini</a>
         </div>
+          @foreach ($months as $key => $month)
+              <a href="{{route('admin.statistics.month', ['id' => $id , 'month' => $key+1 ])}}" class="btn btn-outline-dark">{{$month}}</a>
+          @endforeach
 
         <canvas id="ordersChart" class="mt-5"></canvas>
-        <div class="">
-            <h1>totale ordini {{$sum}}</h1>
+        <div class="mt-2">
+            <h1>Totale ordini &euro; {{$sum}}</h1>
         </div>
       </div>
     </div>

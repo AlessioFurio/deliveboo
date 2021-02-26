7add3a817892f335a9d5f799577df3a698e45b4c
Roberta <robertasciortino.ag@gmail.com>
2021-02-26 09:33:38 +0100

    messaggio carrello vuoto

--- resources/views/guest/payments/index.blade.php
+++ resources/views/guest/payments/index.blade.php
@@ -8,95 +8,89 @@
 @section('content')
 
   <div class="container-fluid">
-    <div class="row g-3">
-      <div class="offset-xs-1 col-xs-10">
-        <div v-if="cartCookie.length" class="row g-3" v-cloak>
-          <div class="offset-xs-1 col-xs-10 col-md-5 col-lg-4 order-md-last mt-5">
-        <h4 class="d-flex justify-content-between align-items-center mb-3">
-          <span>Il tuo carrello</span>
-          {{-- <span class="badge bg-secondary rounded-pill">@{{cartCookie.length}}</span> --}}
-        </h4>
-        <ul class="list-group mb-3">
-          <li v-for="product in cartCookie" :key="product.id" class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">@{{ product.name }}</h6>
-              <span class="cart">
-                <a @click="updateCart(product, 'subtract'), cartBtnLessPlus()">
-                  <i class="far fa-minus-square"></i>
-                </a>
-                <span class="cart__quantity">@{{ product.quantity }}</span>
-                <a @click="updateCart(product, 'add'), cartBtnLessPlus()">
-                  <i class="far fa-plus-square"></i>
-                </a>
-              </span>
-            </div>
-            <span class="text-muted">@{{ product.price }}</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between">
-            <span>Total (USD)</span>
-            <strong>&euro; @{{Math.round(totalPrice * 100)/100}}</strong>
-          </li>
-        </ul>
-      </div>
 
-          <div class="offset-xs-1 col-xs-10 col-md-7 col-lg-8">
-          <form action="{{route('transaction')}}" method="post" class="needs-validation mt-5" @submit="Save">
-            @csrf
-            @method('POST')
-            <h4 class="mb-3">Dati di consegna</h4>
-            <div class="row">
-              <div class="col-sm-12 mb-3">
-                <input v-if="cartCookie.length" type="text" name="price" :value="parseFloat(totalPrice)" hidden>
-                <input v-if="cartCookie.length" type="text" name="restaurant_id" :value="parseFloat(cartCookie[0].restaurant_id)" hidden>
-                <label>Nome</label>
-                <input v-model="nome" type="text" class="form-control" id="firstName" placeholder="Inserisci il tuo nome" value="" name="nome" required>
-                <div class="invalid-feedback">
-                  Valid first name is required.
-                </div>
-                <label>Cognome</label>
-                <input v-model="cognome" type="text" class="form-control" id="lastName" placeholder="Inserisci il tuo cognome" value="" name="cognome" required>
-                <div class="invalid-feedback">
-                  Valid last name is required.
-                </div>
-                <label>Indirizzo</label>
-                <input v-model="indirizzo" type="text" class="form-control" id="address" placeholder="Inserisci l'indirizzo di consegna" name="indirizzo" required>
-                <div class="invalid-feedback">
-                  Please enter your shipping address.
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm-12 mb-3">
-                <div id="dropin-container"></div>
-                 <button class="btn btn-primary btn-lg btn-block" type="submit">Paga ora</button>
-                <input type="hidden" id="nonce" name="payment_method_nonce"/>
-              </div>
-            </div>
-          </form>
+    <div v-if="cartCookie.length" class="row g-3" v-cloak>
+      <div class="offset-xs-1 col-xs-10 col-md-5 col-lg-4 order-md-last mt-5">
+    <h4 class="d-flex justify-content-between align-items-center mb-3">
+      <span>Il tuo carrello</span>
+      {{-- <span class="badge bg-secondary rounded-pill">@{{cartCookie.length}}</span> --}}
+    </h4>
+    <ul class="list-group mb-3">
+      <li v-for="product in cartCookie" :key="product.id" class="list-group-item d-flex justify-content-between lh-sm">
+        <div>
+          <h6 class="my-0">@{{ product.name }}</h6>
+          <span class="cart">
+            <a @click="updateCart(product, 'subtract'), cartBtnLessPlus()">
+              <i class="far fa-minus-square"></i>
+            </a>
+            <span class="cart__quantity">@{{ product.quantity }}</span>
+            <a @click="updateCart(product, 'add'), cartBtnLessPlus()">
+              <i class="far fa-plus-square"></i>
+            </a>
+          </span>
         </div>
-        </div>
+        <span class="text-muted">@{{ product.price }}</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        <span>Total (USD)</span>
+        <strong>&euro; @{{Math.round(totalPrice * 100)/100}}</strong>
+      </li>
+    </ul>
+  </div>
 
-        {{-- modal --}}
-        <div v-else class="row g-3">
-          <div class="col-12">
-            <div class="modal-container">
-              <div class="modal" @click="closeModalOnWindow()">
-                <div class="modal-content">
-                  <span @click="closeModal()" class="close">  &times;</span>
-                  <div class="result">
-                    <i class="fas fa-check-circle fa-9x"></i>
-                    <h2 class="mt-3">
-                      Non hai ancora inserito prodotti nel carrello!
-                    </h2>
-                  </div>
-                </div>
-              </div>
+      <div class="offset-xs-1 col-xs-10 col-md-7 col-lg-8">
+      <form action="{{route('transaction')}}" method="post" class="needs-validation mt-5" @submit="Save">
+        @csrf
+        @method('POST')
+        <h4 class="mb-3">Dati di consegna</h4>
+        <div class="row">
+          <div class="col-sm-12 mb-3">
+            <input v-if="cartCookie.length" type="text" name="price" :value="parseFloat(totalPrice)" hidden>
+            <input v-if="cartCookie.length" type="text" name="restaurant_id" :value="parseFloat(cartCookie[0].restaurant_id)" hidden>
+            <label>Nome</label>
+            <input v-model="nome" type="text" class="form-control" id="firstName" placeholder="Inserisci il tuo nome" value="" name="nome" required>
+            <div class="invalid-feedback">
+              Valid first name is required.
+            </div>
+            <label>Cognome</label>
+            <input v-model="cognome" type="text" class="form-control" id="lastName" placeholder="Inserisci il tuo cognome" value="" name="cognome" required>
+            <div class="invalid-feedback">
+              Valid last name is required.
+            </div>
+            <label>Indirizzo</label>
+            <input v-model="indirizzo" type="text" class="form-control" id="address" placeholder="Inserisci l'indirizzo di consegna" name="indirizzo" required>
+            <div class="invalid-feedback">
+              Please enter your shipping address.
             </div>
           </div>
         </div>
+        <div class="row">
+          <div class="col-sm-12 mb-3">
+            <div id="dropin-container"></div>
+             <button class="btn btn-primary btn-lg btn-block" type="submit">Paga ora</button>
+            <input type="hidden" id="nonce" name="payment_method_nonce"/>
+          </div>
+        </div>
+      </form>
+    </div>
+    </div>
 
+    {{-- modal --}}
+    <div v-else class="row g-3">
+      <div class="col-12">
+        <div class="message-container">
+          <img src="{{asset('images/shopping-cart-empty-side-view.png')}}" alt="">
+          <h2 class="mt-3">
+            Non hai ancora inserito prodotti nel carrello!
+          </h2>
+          <a href="/" class="btn">
+            <i class="fas fa-home"></i>
+            Torna alla home
+          </a>
+        </div>
       </div>
     </div>
+
   </div>
 
 @endsection

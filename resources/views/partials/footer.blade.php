41cf60d1d1936e38af32b8c755668780a9ee2df5
Roberta <robertasciortino.ag@gmail.com>
2021-02-26 19:04:29 +0100

    sistemato app.js

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,38 +3,35 @@
         <div class="container-totale">
             <div class="container-list">
                 <div class="list">
-                    <ul>
-                        <a href="{{ route('welcome') }}"><li>Home</li></a>
-                        <a href="#"><li>Menu</li></a>
-                        <a href="#"><li>Reservation</li></a>
-                        <a href="#"><li>Shop</li></a>
-                        <a href="#"><li>News</li></a>
-                        <a href="{{ route('contacts') }}"><li>Contact</li></a>
-                    </ul>
+
+
+                    <div class="logo">
+                      <img src="{{ asset('images/logo2.png') }}" alt="deliveboo-logo">
+                      <h3>Deliveboo</h3>
+                    </div>
+                  <div class="container-contact">
+                    <div class="contact">
+                      <a href="#">+4 1800 555 1234</a>
+                      <h5>food@deliveboo.com</h5>
+                      <ul>
+                        <li>49 Featherstone Street</li>
+                        <li>London</li>
+                        <li>EC1Y 8SY</li>
+                        <li>United Kingdom</li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
             </div>
             <div class="container-social-online">
                 <div class="social-online">
-                    <h3>Online Reservation</h3>
-                    <span>Follow</span>
+                    <span>Follow us</span>
                     <div class="social">
                         <a href="#"><i class="fab fa-facebook-f"></i></a>
                         <a href="#"><i class="fab fa-instagram"></i></a>
                         <a href="#"><i class="fab fa-twitter"></i></a>
                         <a href="#"><i class="fab fa-snapchat-ghost"></i></a>
                     </div>
-                </div>
-            </div>
-            <div class="container-contact">
-                <div class="contact">
-                    <a href="#">+4 1800 555 1234</a>
-                    <h5>BOOKATABLE@RESTAURANT.COM</h5>
-                    <ul>
-                        <li>49 Featherstone Street</li>
-                        <li>LONDON</li>
-                        <li>EC1Y 8SY</li>
-                        <li>UNITED KINGDOM</li>
-                    </ul>
                 </div>
             </div>
         </div>

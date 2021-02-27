913bf86120ea5b497295dc3c9b762e9ce19301d1
Roberta <robertasciortino.ag@gmail.com>
2021-02-27 12:05:46 +0100

    fine modifiche footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,39 +1,32 @@
 <footer>
-    <div class="container-footer">
-        <div class="container-totale">
-            <div class="container-list">
-                <div class="list">
 
+    <div class="container-contact">
+      <p>+4 1800 555 1234</p>
+      <h5>food@deliveboo.com</h5>
+      <ul>
+        <li>49 Featherstone Street</li>
+        <li>London</li>
+        <li>EC1Y 8SY</li>
+        <li>United Kingdom</li>
+      </ul>
+    </div>
+    <div class="container-social-online">
+      <div class="logo">
+        <img src="{{ asset('images/logo2.png') }}" alt="deliveboo-logo">
+        <h1>Deliveboo</h1>
+      </div>
+      <h2>Follow us</h2>
+      <div class="social">
+          <a href="#"><i class="fab fa-facebook-f fa-2x"></i></a>
+          <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
+          <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fab fa-snapchat-ghost fa-2x"></i></a>
+      </div>
+    </div>
 
-                  <div class="container-contact">
-                    <div class="contact">
-                      <a href="#">+4 1800 555 1234</a>
-                      <h5>food@deliveboo.com</h5>
-                      <ul>
-                        <li>49 Featherstone Street</li>
-                        <li>London</li>
-                        <li>EC1Y 8SY</li>
-                        <li>United Kingdom</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-            </div>
-            <div class="container-social-online">
-                <div class="social-online">
-                  <div class="logo">
-                    <img src="{{ asset('images/logo2.png') }}" alt="deliveboo-logo">
-                    <h3>Deliveboo</h3>
-                  </div>
-                    <span>Follow us</span>
-                    <div class="social">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-snapchat-ghost"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="store">
+      <a href="#">
+        <img src="images/app-loghi.png" alt="">
+      </a>
     </div>
 </footer>

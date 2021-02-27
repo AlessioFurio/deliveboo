bbc7c890db668128da4ebee0e2b50dda4c993d22
Roberta <robertasciortino.ag@gmail.com>
2021-02-27 12:13:15 +0100

    modifiche footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,42 +1,4 @@
 <footer>
-<<<<<<< HEAD
-    <div class="container-footer">
-        <div class="container-totale">
-            <div class="container-list">
-                <div class="list">
-
-
-                    <div class="logo">
-                      <img src="{{ asset('images/logo2.png') }}" alt="deliveboo-logo">
-                      <h3>Deliveboo</h3>
-                    </div>
-                  <div class="container-contact">
-                    <div class="contact">
-                      <a href="#">+4 1800 555 1234</a>
-                      <h5>food@deliveboo.com</h5>
-                      <ul>
-                        <li>49 Featherstone Street</li>
-                        <li>London</li>
-                        <li>EC1Y 8SY</li>
-                        <li>United Kingdom</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-            </div>
-            <div class="container-social-online">
-                <div class="social-online">
-                    <span>Follow us</span>
-                    <div class="social">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-snapchat-ghost"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-=======
 
     <div class="container-contact">
       <p>+4 1800 555 1234</p>
@@ -66,6 +28,5 @@
       <a href="#">
         <img src="images/app-loghi.png" alt="">
       </a>
->>>>>>> footer
     </div>
 </footer>

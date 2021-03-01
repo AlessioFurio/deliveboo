8b1b79ea6295b65161721824108b24982ecb9e74
Roberta <robertasciortino.ag@gmail.com>
2021-03-01 11:28:22 +0100

    sistemato come ordinare restaurant

--- resources/views/guest/restaurants/show.blade.php
+++ resources/views/guest/restaurants/show.blade.php
@@ -1,74 +1,72 @@
 @extends('layouts.app-guest')
 @section('content')
 
-        <main>
-            <div class="how-to-order-restaurant">
-                <h1>
-                  Come ordinare:
-                </h1>
-                <div class="steps">
-                  <div class="step">
-                    <img src="{{url('/images/menu.png')}}" alt="">
-                    <h5>
-                      1. Scegli il ristorante
-                    </h5>
-                  </div>
-                  <div class="step">
-                    <img src="{{url('/images/order-food.png')}}" alt="">
-                    <h5>
-                      2. Seleziona i tuoi piatti preferiti
-                    </h5>
-                  </div>
-                  <div class="step">
-                    <img src="{{url('/images/food-delivery.png')}}" alt="">
-                    <h5>
-                      3. Attendi la consegna
-                    </h5>
-                  </div>
-                </div>
-              </section>
+  <main>
+    <section class="how-to-order">
+      <h1>
+        Come ordinare:
+      </h1>
+      <div class="steps">
+        <div class="step">
+          <img src="{{url('/images/menu.png')}}" alt="">
+          <h5>
+            1. Scegli il ristorante
+          </h5>
+        </div>
+        <div class="step">
+          <img src="{{url('/images/order-food.png')}}" alt="">
+          <h5>
+            2. Seleziona i tuoi piatti preferiti
+          </h5>
+        </div>
+        <div class="step">
+          <img src="{{url('/images/food-delivery.png')}}" alt="">
+          <h5>
+            3. Attendi la consegna
+          </h5>
+        </div>
+      </div>
+    </section>
+    <div class="container-restaurant">
+      <div class="content-restaurant">
+        <div class="title-restaurant">
+            <h1>
+              {{$restaurant->name}}
+            </h1>
+        </div>
+        <div class="details-restaurant">
+            <h4>Info ristorante:</h4>
+            <p>
+                <i class="fas fa-map-marker-alt"></i>
+                Indirizzo: <br>{{$restaurant->address}}
+            </p>
+            <p>
+                <i class="fas fa-phone-alt"></i>
+                Telefono: <br>{{$restaurant->phone}}
+            </p>
+        </div>
+      </div>
+
+      <div class="container-dish">
+        <div class="container-list-dishes">
+          <div v-for="product in dishesList" :key="product.id" class="card-dish">
+            <h3 class="product__header">@{{ product.name }}</h3>
+            <div class="wp-img-card-dish">
+                <img v-if="product.cover == null" src="{{url('/images/card.jpg')}}" alt="">
+                <img v-else :src="'{{url ('/storage')}}' + '/' + product.cover" :alt="product.name" class="product__image">
             </div>
-            <div class="container-restaurant">
-                <div class="content-restaurant">
+            <p class="product__description">@{{ product.description }}</p>
+            <p class="">Prezzo: @{{ product.price }} €</p>
 
-                    <div class="title-restaurant">
-                        <h1>
-                          {{$restaurant->name}}
-                        </h1>
-                    </div>
-                    <div class="details-restaurant">
-                        <h4>Info ristorante:</h4>
-                        <p>
-                            <i class="fas fa-map-marker-alt"></i>
-                            Indirizzo: <br>{{$restaurant->address}}
-                        </p>
-                        <p>
-                            <i class="fas fa-phone-alt"></i>
-                            Telefono: <br>{{$restaurant->phone}}
-                        </p>
-                    </div>
-                </div>
-
-                <div class="container-dish">
-                    <div class="container-list-dishes">
-                        <div v-for="product in dishesList" :key="product.id" class="card-dish">
-                            <h3 class="product__header">@{{ product.name }}</h3>
-                            <div class="wp-img-card-dish">
-                                <img v-if="product.cover == null" src="{{url('/images/card.jpg')}}" alt="">
-                                <img v-else :src="'{{url ('/storage')}}' + '/' + product.cover" :alt="product.name" class="product__image">
-                            </div>
-                            <p class="product__description">@{{ product.description }}</p>
-                            <p class="">Prezzo: @{{ product.price }} €</p>
-
-                            <div class="cart">
-                                <button @click="updateCart(product, 'subtract'), cartBtnLessPlus()" class="cart__button">-</button>
-                                <span v-if="totalPrice != 0" class="cart__quantity">@{{ product.quantity }}</span>
-                                <span v-else class="cart__quantity">0</span>
-                                <button @click="updateCart(product, 'add'), cartBtnLessPlus()" class="cart__button">+</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="cart">
+                <button @click="updateCart(product, 'subtract'), cartBtnLessPlus()" class="cart__button">-</button>
+                <span v-if="totalPrice != 0" class="cart__quantity">@{{ product.quantity }}</span>
+                <span v-else class="cart__quantity">0</span>
+                <button @click="updateCart(product, 'add'), cartBtnLessPlus()" class="cart__button">+</button>
             </div>
-        </main>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 @endsection

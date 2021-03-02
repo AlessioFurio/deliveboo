eeaf0dc8ba9455c6c5d78c05250a46cb1b4d85a0
degorts <danilodegortes@gmil.com>
2021-03-02 11:19:34 +0100

    logo + stat

--- resources/views/admin/statistics/month.blade.php
+++ resources/views/admin/statistics/month.blade.php
@@ -23,7 +23,7 @@
         <canvas id="ordersChart" class="mt-5"></canvas>
         <div class="mt-2">
             <h1>Totale ordini &euro; {{$sum}}</h1>
-            <h3>Numero ordini  {{$num}}</h3>
+            <h3>Numero ordini {{$num}}</h3>
         </div>
       </div>
     </div>
@@ -39,6 +39,8 @@
       window.cData = JSON.parse(`@php
         echo $chart_data;
       @endphp `);
+
+
 
 
     </script>

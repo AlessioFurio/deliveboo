0052716c5a7379d441c89e71dd0b298f12c91ca2
Manuel <manuel.devito@icloud.com>
2021-03-03 00:32:37 +0100

    iniziata pagina reset password...modifca pagina aggiungi ristorante

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-                
-                <div class="card-body">
+{{-- <div class="container"> --}}
+    <div class="container-total-recovery-password">
+        <div class="total-recovery-password">
+            <div class="card-recovery-password">
+                <div class="card-header-recovery-password">{{ __('Login Cliente') }}</div>
+                <div class="text-recovery-password">
+                    <p>Inserisci il tuo indirizzo email qui sotto per ricevere un link di reimpostazione della password.</p>
+                </div>
+                <div class="card-body-recovery-password">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -17,11 +19,12 @@
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form-recovery-password">
+                            <label for="email" class="label-recovery-password">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="container-input-recovery-password">
+                                <input id="email" type="email" placeholder="Inserisci la email..." class="form-input-recovery-password
+                                @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -31,17 +34,15 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
+                        <div class="container-button-recovery-password">
+                            <button type="submit" class="button-recovery-password">
+                                {{ __('Reset Password') }}
+                            </button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
+{{-- </div> --}}
 @endsection
